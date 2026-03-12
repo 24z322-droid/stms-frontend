448de1dd60382ad9e44983be4581db8895420ed6
24z322-droid <24z322@psgtech.ac.in>
2026-03-12 21:57:51 +0530

    Update route.js

--- js/route.js
+++ js/route.js
@@ -1,1 +1,25 @@
+const API="https://stms-backend.onrender.com"
 
+async function findRoute(){
+
+let from=document.getElementById("from").value
+
+let to=document.getElementById("to").value
+
+let res=await fetch(API+"/route",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({from,to})
+
+})
+
+let data=await res.json()
+
+document.getElementById("result").innerText="Best Route: "+data.route
+
+}
