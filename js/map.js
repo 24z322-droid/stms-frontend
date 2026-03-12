33dd470baf4e6866e26692c8b8527368372a4b69
24z322-droid <24z322@psgtech.ac.in>
2026-03-12 21:57:37 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,12 @@
+let map=L.map("map").setView([13.0827,80.2707],12)
 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+
+L.marker([13.0827,80.2707]).addTo(map)
+.bindPopup("Traffic Zone A")
+
+L.marker([13.05,80.25]).addTo(map)
+.bindPopup("Traffic Zone B")
+
+L.marker([13.10,80.29]).addTo(map)
+.bindPopup("Traffic Zone C")
