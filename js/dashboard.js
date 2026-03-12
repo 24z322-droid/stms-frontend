57f23062b9b3175657b8d4e5f57170298eca851f
24z322-droid <24z322@psgtech.ac.in>
2026-03-12 21:57:20 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,17 @@
+const API="https://stms-backend.onrender.com"
 
+async function loadTraffic(){
+
+let res=await fetch(API+"/traffic")
+
+let data=await res.json()
+
+document.getElementById("roadA").innerText="Road A : "+data.roadA
+
+document.getElementById("roadB").innerText="Road B : "+data.roadB
+
+document.getElementById("roadC").innerText="Road C : "+data.roadC
+
+}
+
+loadTraffic()
